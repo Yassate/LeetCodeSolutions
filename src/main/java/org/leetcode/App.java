d8ab95bf7ead758fd71b78768e6f90545844973a
Yassate <bartosz.walawender@gmail.com>
2023-06-20 17:49:57 +0200

    Minor

--- src/main/java/org/leetcode/App.java
+++ src/main/java/org/leetcode/App.java
@@ -1,14 +1,8 @@
 package org.leetcode;
 
-/**
- * Hello world!
- *
- */
 public class App
 {
     public static void main( String[] args )
     {
-        Solution004 solutions = new Solution004();
-
     }
 }
